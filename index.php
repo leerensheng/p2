15f09c615076bdf83703200a3bfd43399281ee4e
Matt Lee <matthewbrentlee@g.harvard.edu>
2016-02-16 08:13:58 +0800

    v1, but has a problem of repeating words.

--- index.php
+++ index.php
@@ -4,11 +4,29 @@
 	<meta charset="UTF-8">
 	<title>Password Generator</title>
   <link rel="stylesheet" href="style.css">
-  <?php require("logic.php"); ?>
+  <?php require "logic.php"; ?>
 </head>
 <body>
-	<form action="pwgenerator.php" method="GET">
+	<form action="index.php" method="GET">
     <input type="text" name="numberOfWords">
+		<br>
+		<input type="checkbox" name="useNumber">Would you like to use numbers?
+		<br>
+		<input type="checkbox" name="useSymbol">Would you like to use symbols?
+		<br>
+		<input type="submit" value="Generate Password">
+
+		<?php
+		##$result = array_rand($dictionaryWords, $howmany);
+		?>
+		<?php
+		for ($i = 0; $i <= $howmany; $i++) {
+			$randomWord = array_rand($dictionaryWords);
+			$result = $dictionaryWords[$randomWord];
+			echo $result . "-";
+		}
+		?>
+
   </form>
 </body>
 </html>
---
+++ logic.php
@@ -0,0 +1,30 @@
+<?php
+
+  $dictionaryWords = array("guitar", "monument", "bus", "department", "city", "overpass", "communication", "drive", "camoflage", "hostel", "dispense", "countdown", "northwest", "look", "friendship", "reduce", "carefully", "guard", "repel", "vastly");
+
+  $howmany = $_GET["numberOfWords"];
+  $howmany = $howmany - 1;
+
+/*  if $useNumber == "on" {
+    echo "We can use numbers";
+  }
+  else {
+    echo "We won't use numbers";
+  }
+
+  if $useSymbol == "on" {
+    echo "We can use symbols";
+  }
+  else {
+    echo "We won't use symbols";
+  }
+*/
+
+##  if $numberOfWords < 10 && $numberOfWords > 3 {
+##    echo "That's good!";
+##  }
+##  else {
+##  echo "Please enter a number between 4 and 10."
+##  }
+
+?>
